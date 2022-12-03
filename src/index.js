a48897ee1c1c4b9128d60e32c1076b7ee499eac8
patrick kisyula <pmusyimi42@gmail.com>
2022-12-03 20:26:46 +0300

    Done with assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  form = document.querySelector("form")
+  form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const newTask = document.getElementById('new-task-description').value
+    const list = document.createElement("li");
+    list.innerText = newTask
+    const task = document.getElementById("tasks")
+    task.appendChild(list)
+    const btn = document.createElement("button");
+    btn.innerText = " X "
+    btn.addEventListener("click", () =>{
+      task.removeChild(list)
+    })
+    list.appendChild(btn)
+  });
+  
 });
